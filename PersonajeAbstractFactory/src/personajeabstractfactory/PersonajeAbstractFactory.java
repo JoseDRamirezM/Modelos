9f8b3e1d4ce35a8a4a3ba78c9473466b4f1ee3b9
JoseDRamirezM <eskatejose@hotmail.com>
2019-09-13 21:32:55 -0500

    z

---
+++ PersonajeAbstractFactory/src/personajeabstractfactory/PersonajeAbstractFactory.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personajeabstractfactory;
+
+import Vista.Ventana;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class PersonajeAbstractFactory {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana v1 = new Ventana();
+    }
+    
+}
